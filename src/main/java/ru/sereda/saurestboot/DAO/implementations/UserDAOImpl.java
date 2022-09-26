9a46b8f6c91bd616089a34410b4613d76b297de3
Михаил <mikhsereda@gmail.com>
2022-09-26 18:27:28 +1000

    using count of rows

--- src/main/java/ru/sereda/saurestboot/DAO/implementations/UserDAOImpl.java
+++ src/main/java/ru/sereda/saurestboot/DAO/implementations/UserDAOImpl.java
@@ -16,12 +16,12 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User save(User user) {
-//        String sql2 = "SELECT count(*) FROM users WHERE id = ?";
-//        int count = jdbcTemplate.queryForObject(sql2,Integer.class,user.getId());
-//        if (count>0){
-        String sql = "SELECT * FROM users WHERE id=?";
-        List<User> users = jdbcTemplate.query(sql, new UserMapper(),user.getId());
-        if (users.size()>0){
+        String sql2 = "SELECT count(*) FROM users WHERE id = ?";
+        int count = jdbcTemplate.queryForObject(sql2,Integer.class,user.getId());
+        if (count==1){
+//        String sql = "SELECT * FROM users WHERE id=?";
+//        List<User> users = jdbcTemplate.query(sql, new UserMapper(),user.getId());
+//        if (users.size()>0){
             String sqlUpdate = "UPDATE public.users SET name=?, password=?, description=? WHERE id=?;";
             jdbcTemplate.update(sqlUpdate,user.getUsername(),user.getPassword(),user.getDescription(),user.getId());
         }
