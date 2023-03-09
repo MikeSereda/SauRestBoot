6811b4044a9d4c78af426ab131ace454e551ee0a
Михаил <mikhsereda@gmail.com>
2023-03-10 01:12:28 +1000

    Added eureka-client possibilities

--- src/main/java/ru/sereda/saurestboot/SauRestBootApplication.java
+++ src/main/java/ru/sereda/saurestboot/SauRestBootApplication.java
@@ -2,8 +2,10 @@ package ru.sereda.saurestboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"ru.sereda.saurestboot"})
+@EnableDiscoveryClient
 public class SauRestBootApplication {
 
     public static void main(String[] args) {
