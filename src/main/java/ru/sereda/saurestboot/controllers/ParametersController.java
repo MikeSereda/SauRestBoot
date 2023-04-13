b1899c5b9cc2c36fa5c58708c8c88be74835dca2
Михаил <mikhsereda@gmail.com>
2023-04-13 19:26:46 +1000

    StartTime is unnecessary

--- src/main/java/ru/sereda/saurestboot/controllers/ParametersController.java
+++ src/main/java/ru/sereda/saurestboot/controllers/ParametersController.java
@@ -17,13 +17,6 @@ public class ParametersController {
     @Autowired
     ParametersService parametersService;
 
-    // TODO: 10.04.2023 select timestamp_wotz, avg as eb_no from
-    //    (select timestamp_wotz, avg(eb_no) over (partition by t.row) as avg, row, lag(row) over () as row_lag from
-    //     (SELECT timestamp_wotz, eb_no, (row_number() over () -1)/5 as row FROM public.parameters
-    //       WHERE device_id='cdm111' AND timestamp_wotz<'2023-04-10 04:57:45' AND timestamp_wotz>'2023-04-10 04:40:05' ORDER BY timestamp_wotz)
-    //          as T order by T.row ) as D where row<>row_lag
-    //  query for approx params
-
     @GetMapping("/parameters")
     public Map<String, List<Parameters>> testGetParameters(
             @RequestParam(name = "startTime", required = false, defaultValue = "") String startTime,
@@ -41,7 +34,7 @@ public class ParametersController {
 
     @PostMapping("/parameters")
     public Map<String, List<Parameters>> testPostParameters(
-            @RequestParam(name = "startTime") String startTime,
+            @RequestParam(name = "startTime", required = false, defaultValue = "") String startTime,
             @RequestParam(name = "endTime", required = false, defaultValue = "") String endTime,
             @RequestParam(name = "deviceId", required = false, defaultValue = "") String deviceId,
             @RequestParam
